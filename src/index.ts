d65a0a52663aa3b27a755118f06bd3e4a5d2ec5d
Hugo Brosnahan <101022007+hugo-t-b@users.noreply.github.com>
2022-05-28 10:19:34 +1000

    feat: Add rules

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { ESLintConfig } from "./types";
+import rules from "./rules.json";
+
+const config: ESLintConfig = {
+  rules
+};
+
+export default config;
---
+++ src/types.ts
@@ -0,0 +1,4 @@
+export interface ESLintConfig {
+  env?: object,
+  rules?: object
+};
